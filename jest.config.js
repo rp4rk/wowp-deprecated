cf3117c58c14a1ab7d858e55a542fca5acfc463f
Ryan Park <admin@ryanpark.co.uk>
2020-02-15 16:19:48 +0000

    Configuration for absolute imports

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   roots: ["<rootDir>/src"],
+  moduleDirectories: ["node_modules", "src"],
   testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest"
--- src/lib/combatlogevents.types.ts
+++
@@ -1,1 +0,0 @@
-export type CombatLogEvent = CombatLogEventBase | CombatLogVersionEvent | CombatLogEncounterEvent;
